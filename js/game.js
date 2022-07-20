3089e9e37c459d162426d52201f554605d38f7eb
Yener88 <106224876+Yener88@users.noreply.github.com>
2022-07-20 07:31:46 +0200

    mittwoch 1.4

--- js/game.js
+++ js/game.js
@@ -37,3 +37,15 @@ addEventListener('keydown', (event) => {
         }
     };
 })
+
+function checkOrientation() {
+    if (window.matchMedia("(orientation: landscape)").matches) {
+        if (window.innerHeight < 480) {
+            newHeight = window.innerHeight;
+            document.getElementById('canvas').style.height = `${newHeight}px`;
+        }
+    }
+    else {
+        document.getElementById('canvas').style.height = `100%`;
+    }
+}
